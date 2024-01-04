fa40311f4164b633e029f4027622914ec785232b
Ashutosh <ashu.ydv2001@gmail.com>
2024-01-04 20:57:03 +0530

    bhai sacchi complete hogaya

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messageRoute");
+const socket = require("socket.io");
 
 const app = express();
 require("dotenv").config();
@@ -25,3 +26,26 @@ app.use("/api/messages/", messageRoutes);
 const server = app.listen(process.env.PORT, () => {
   console.log("server connected");
 });
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on('connection', (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id)
+  })
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket= onlineUsers.get(data.to);
+    if(sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message)
+    }
+  })
+})
